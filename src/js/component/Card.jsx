a5cca23ecada20f23226b5b504ef027776e01ec7
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-10-06 04:15:39 +0000

    Card is complete

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,21 +1,49 @@
 import React from "react";
+import businessman from "../../img/businessman.jpeg";
 
 const Card = (props) => {
   const { person } = props;
   const { full_name, address, phone, email } = person;
 
   return (
-    <div className="col-12 w-60">
-      <div className="card">
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{full_name}</h5>
-          <p className="card-text">{address}</p>
-          <p className="card-text">{phone}</p>
-          <p className="card-text">{email}</p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+    <div className="card mb-3 col-12 w-80">
+      <div className="row g-0">
+        <div className="col-md-4">
+          <img
+            src={businessman}
+            className="img-fluid rounded-circle mx-auto pt-3 d-block"
+            alt="businessman"
+            width={150}
+          />
+        </div>
+        <div className="col-md-6">
+          <div className="card-body">
+            <h4 className="card-title">{full_name}</h4>
+            <p className="card-text text-secondary">
+              <span className="mx-2">
+                <i className="fas fa-map-marker-alt" />
+              </span>
+              {address}
+            </p>
+            <p className="card-text text-secondary">
+              <span className="mx-2">
+                <i className="fas fa-phone" />
+              </span>
+              {phone}
+            </p>
+            <p className="card-text text-secondary">
+              <span className="mx-2">
+                <i className="fas fa-envelope"></i>
+              </span>
+              {email}
+            </p>
+          </div>
+        </div>
+        <div className="col-md-2 card-buttons d-flex justify-content-around pt-3">
+          <span>
+            <i className="fas fa-pencil-alt border border-secondary rounded-circle p-2 me-2"></i>
+            <i className="fas fa-trash border border-secondary rounded-circle p-2 me-2"></i>
+          </span>
         </div>
       </div>
     </div>
